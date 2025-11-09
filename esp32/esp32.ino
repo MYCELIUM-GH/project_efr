f3dd8a30008b6d7a56e6ef746f5a613cf62eb46e
Oleh <108955160+MYCELIUM-GH@users.noreply.github.com>
2025-11-09 19:51:59 +0000

    Update WiFi and MQTT configurations in esp32.ino

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -6,34 +6,46 @@ Whole code follows logic "sensor->wifi->mqtt", so it should be easy to read.
 =====================================================================================
 Made by Oleh
 */
-#include "DHT.h"
-#include <WiFi.h>
-#include <PubSubClient.h>
-// SENSOR CONFIG
-#define DHTPIN 4
+// ==== ==== IMPORTS ==== ==== 
+#include "DHT.h" // library for sensor
+#include <WiFi.h> // library for wifi connection
+#include <PubSubClient.h> // library for MQTT (God, i love programming - just use libraries, that's all)
+// ==== ==== SENSOR CONF ==== ====
+// sensor's data pin connected to D4 on ESP32
+#define DHTPIN 4 
+// type of sensor, because the library supports several
 #define DHTTYPE DHT11
+// it defines dht as DHT type variable with pin4 and DHT11 macros which i created above
 DHT dht(DHTPIN, DHTTYPE);
-// WIFI CONFIG
-const char WIFI_SSID[] = "House Admin";
-const char WIFI_PASS[] = "gr34TWif1!!";
-const char MQTT_BROKER[] = "192.168.29.104";
+// ==== ==== WIFI CONF ==== ==== 
+// name of network, password, server address and credits to login. i used mobile hotspot because my home network is wild
+const char WIFI_SSID[] = "Arch";
+const char WIFI_PASS[] = "bhfbkchwj";
+const char MQTT_BROKER[] = "10.19.14.156";
 const char MQTT_TOPIC[] = "esp32/dht11";
-// MQTT CONFIG
+const char* MQTT_USER = "esp32user";
+const char* MQTT_PASS = "1337";
+// ==== ==== MQTT CONF ==== ==== 
+// basic declarations, don't know much, just used guide. but probably defines espClient as macro
 WiFiClient espClient;
+// same as dht: new variable
 PubSubClient client(espClient);
+// initialisation of var for data messege
 unsigned long lastMsg = 0;
+// was neccecary to prevent server from storing too much data
 #define MSG_BUFFER_SIZE  (50)
 char msg[MSG_BUFFER_SIZE];
-
-void setup_wifi() 
+// ==== ==== WIFI FUNCTION ==== ==== 
+void setup_wifi()
+// separate function for wireless connection
 {
     delay(2000);
     Serial.println();
     Serial.print("Connecting to ");
     Serial.println(WIFI_SSID);
-
+    // starts connection with defined creditations
     WiFi.begin(WIFI_SSID, WIFI_PASS);
-
+    // sort of stop until esp connects to wifi, added mostly for debugging (and it's also fancy)
     while(WiFi.status() != WL_CONNECTED) 
     {
         delay(1000);
@@ -44,22 +56,27 @@ void setup_wifi()
     Serial.println("Connected!");
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
+    // if successful - assigns integrated LED to output mode, i'll explain later
     pinMode(2, OUTPUT);
 }
-
-void reconnect_mqtt() 
+// ==== ==== MQTT RECONNECT FUNCTION ==== ==== 
+void reconnect_mqtt()
+// mainly checks for successful connection to broker
 {
-    while(!client.connected()) 
+    while(!client.connected())
+    // starts loop similar to what you've seen for wifi
     {
         Serial.println();
         Serial.print("Attempting MQTT connection...");
         String clientId = "ESP32Client-";
+        // assigns random suffix to ESP board, cool feature - i think
         clientId += String(random(0xffff), HEX);
-
-        if(client.connect(clientId.c_str())) 
+        // check for successful connection, takes ID from above as parameter
+        if(client.connect(clientId.c_str(), MQTT_USER, MQTT_PASS)) 
         {
-            Serial.println("connected");
-        } 
+            Serial.println("Connected!");
+        }
+        // in case connection is faled - will display error with code. debugging!
         else 
         {
             Serial.println();
@@ -69,48 +86,57 @@ void reconnect_mqtt()
         }
     }
 }
-
+// ==== ==== MICS FUNCTION ==== ==== 
 void setup() 
 {
+    // shows all the text on band 9600
     Serial.begin(9600);
+    // invokes the function from sensor's lib
     dht.begin();
+    // and wifi setup
     setup_wifi();
+    // defines server's address and port 
     client.setServer(MQTT_BROKER, 1883);
 }
-
+// ==== ==== MAIN LOOP ==== ==== 
 void loop() 
 {
+    // waits 2 seconds between assigning readings from sensor to variables
     delay(2000);
-
+    // that's simple
     float h = dht.readHumidity();
     float t = dht.readTemperature();
-
+    // in case of wrong readings shows error
     if(isnan(h) || isnan(t)) 
     {
         Serial.println(F("Failed to read from DHT sensor!"));
         return;
     }
+    // debugging again, shows readings in serial monitor
     Serial.println();
     Serial.print(F("Humidity: "));
     Serial.print(h);
     Serial.print(F("%  Temperature: "));
     Serial.print(t);
     Serial.print(F("°C "));
-
+    // my favourite part - if esp connected to wifi - it'll light blue LED. FANCY:D
     if(WiFi.status() == WL_CONNECTED)
     {
         digitalWrite(2,HIGH);
     }
-
-    if (!client.connected()) 
+    // invoke function for reconnection to server
+    if (!client.connected())
     {
         reconnect_mqtt();
     }
+    // invokes the function from mqtt's lib
     client.loop();
-
+    // creates json payload
     snprintf(msg, MSG_BUFFER_SIZE, "{\"temperature\":%.1f, \"humidity\":%.1f}", t, h);
+    // and some debugging again
     Serial.println();
     Serial.print("Sending message: ");
     Serial.println(msg);
+    // finally, it sends messege to the server
     client.publish(MQTT_TOPIC, msg);
 }
